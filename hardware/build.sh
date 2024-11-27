59eda6bf50d9ba4d004e44fd1e18189fdc6568cf
jiangnan7 <jiangnan.stars@gmail.com>
2024-11-27 21:24:56 +0800

    Add hardware build info.

--- hardware/build.sh
+++ hardware/build.sh
@@ -3,7 +3,6 @@ cd ..
 mkdir hardfloat
 cd hardfloat 
 git clone https://github.com/ucb-bar/berkeley-hardfloat.git && cd berkeley-hardfloat
-mv * ../ && rm -rf berkeley-hardfloat
 git reset --hard 70455e53f233a06cb5a342d125e22b7b1505c271
 sbt "publishLocal"
-
+mv * ../ && rm -rf berkeley-hardfloat
